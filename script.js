2cc4630ecba021f52591beee7ed471a3741f5968
Kevin Wagner <kevin_wagner4493@web.de>
2022-09-25 22:32:26 +0200

    added add-task dialog

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function init() {
     includeHTML();
 }
+
+function openSummary() {
+    document.getElementById('add-task').classList.add('d-none')
+    document.getElementById('board').classList.add('d-none')
+    document.getElementById('summary').classList.remove('d-none')
+}
+
+function openBoard() {
+    document.getElementById('add-task').classList.add('d-none')
+    document.getElementById('summary').classList.add('d-none')
+    document.getElementById('board').classList.remove('d-none')
+
+
+}
+
+function openAddTask() {
+    document.getElementById('summary').classList.add('d-none')
+    document.getElementById('board').classList.add('d-none')
+    document.getElementById('add-task').classList.remove('d-none')
+}
+
+function openAddTaskDialog() {
+    document.getElementById('add-task-dialog').classList.remove('d-none');
+}
