01621ae102fbe3d61d20f9afc9f8a96ca67b67a5
Kevin Wagner <kevin_wagner4493@web.de>
2022-10-27 20:48:02 +0200

    added realtime clock to summary

--- js/summary.js
+++ js/summary.js
@@ -5,6 +5,7 @@ async function initSummary() {
     await loadAllTasks();
     loadAllCounters();
     activeSummaryNavLink();
+    greetingUsers();
 }
 
 
@@ -112,4 +113,54 @@ function showNearestUrgentDate() {
 
 function activeSummaryNavLink() {
     document.getElementById('summary-link').classList.add('active-link')
+}
+
+
+function greetingUsers() {
+    let myDate = new Date()
+    let hours = myDate.getHours();
+    let greetingMessage;
+
+    if (hours < 12) {
+        greetingMessage = 'Good morning,';
+    } else if (hours <= 12 && hrs <= 17) {
+        greetingMessage = 'Good afternoon,';
+    } else if (hours >= 17 && hours <= 24) {
+        greetingMessage = 'Good evening,';
+    }
+
+    document.getElementById('welcome-text').innerHTML = greetingMessage;
+
+    // setCurrentDate();
+    setCurrentTime();
+}
+
+
+function setCurrentDate() {
+    let currentDate = new Date().toLocaleString("default", {
+        "year": "numeric",
+        "month": "long",
+        "day": "numeric",
+    });
+
+    document.getElementById('date').innerHTML = currentDate;
+}
+
+
+function setCurrentTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(setCurrentTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
 }
