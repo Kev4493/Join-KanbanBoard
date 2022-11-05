82dd9217c9fd731161d8ecd52f00d404a9387e99
Kevin Wagner <kevin_wagner4493@web.de>
2022-11-05 19:07:59 +0100

    contact section edited

--- js/contacts.js
+++ js/contacts.js
@@ -13,23 +13,43 @@ let allContacts = [
     {
         'name': 'Kristian Huptas',
         'email': 'kristian.huptas@icloud.com',
-        'phone': '+49 4584 978 22 6'
+        'phone': '+49 4584 978 22 6',
+        'color': getRandomColor()
     },
     {
         'name': 'Kevin Wagner',
         'email': 'kevin.wagner@icloud.com',
-        'phone': '+49 5646 684 87 3'
+        'phone': '+49 5646 684 87 3',
+        'color': getRandomColor()
     },
     {
         'name': 'Gergana Ivanova',
         'email': 'gergana.ivanova@icloud.com',
-        'phone': '+49 3153 776 21 8'
-    }
-]
+        'phone': '+49 3153 776 21 8',
+        'color': getRandomColor()
+    },
+    {
+        'name': 'Maurice Rauffmann',
+        'email': 'maurice.rauffmann@icloud.com',
+        'phone': '+49 6671 557 97 0',
+        'color': getRandomColor()
+    },
+    {
+        'name': 'Delong Liang',
+        'email': 'delong.liang@icloud.com',
+        'phone': '+49 9641 219 34 4',
+        'color': getRandomColor()
+    },
+    {
+        'name': 'Fabio Berni',
+        'email': 'fabio.berni@icloud.com',
+        'phone': '+49 7233 258 47 6',
+        'color': getRandomColor()
+    },
+];
 
 
 function getInitial() {
-
     // Alle Namen herausfinden:
     for (let i = 0; i < allContacts.length; i++) {
         let names = allContacts[i]['name'];
@@ -45,9 +65,8 @@ function getInitial() {
     // Anfangsbuchstaben Alphabetisch sortieren:
     allInitials.sort();
 
-    // Duplikate entfernen
+    // Duplikate entfernen:
     allInitials = [...new Set(allInitials)]
-    console.log(allInitials);
 }
 
 
@@ -58,9 +77,9 @@ function showAllContacts() {
 
     for (let i = 0; i < allInitials.length; i++) {
         contactsNavContainer.innerHTML += renderContactBoxes(i);
-    }
 
-    checkNames();
+        filterNames(i);
+    }
 }
 
 
@@ -71,30 +90,46 @@ function renderContactBoxes(i) {
                 <p>${allInitials[i]}</p>
             </div>
             <hr>
-            <div id="contact-box" class="contact-section">
-                <p id="name">Name</p>
-                <p>Email</p>
+            <div id="contact-section${i}" class="contact-section">
+                
             </div>
         </div>
     `;
 }
 
 
-function checkNames() {
-    // for (let i = 0; i < allInitials.length; i++) {
-    //     let findNames = allNames.filter((name) => name.startsWith(`${allInitials[0]}`));
-    //     console.log(findNames);
-    // }
+function filterNames(i) {
+    for (let j = 0; j < allNames.length; j++) {
 
-    for (let i = 0; i < allNames.length; i++) {
-
-        if (allNames[i].charAt(0) == allInitials[i]) [
-            document.getElementById('name').innerHTML = /*html*/ `
-                ${allNames[i]}
-            `
-        ]
+        if (allNames[j].charAt(0) === allInitials[i]) {
+            document.getElementById(`contact-section${i}`).innerHTML += renderContactToInitials(j);
+        }
     }
+}
 
+
+function renderContactToInitials(j) {
+    return /*html*/ `
+        <div class="single-contact">
+            <div class="contact-circle" style="background-color: ${allContacts[j]['color']}">
+                <p>${allContacts[j]['name'].split(" ").map(word => word[0]).join("")}</p>
+            </div>
+            <div class="contact-information">
+                <p>${allContacts[j]['name']}</p>
+                <p class="email">${allContacts[j]['email']}</p>
+            </div>
+        </div>
+    `;
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 
