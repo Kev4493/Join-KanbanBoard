4973188995c9f69b7771ac9ed909d60f02b3d3c8
Kevin Wagner <kevin_wagner4493@web.de>
2022-11-25 22:20:28 +0100

    login update

--- js/login.js
+++ js/login.js
@@ -1,9 +1,12 @@
-let users = [
-    {
-        'email': 'kevin_wagner4493@web.de', 
-        'password': 'test123'
-    }
-]
+async function initLogin() {
+    includeHTML();
+    setURL('https://kanbanboard.kev-wagner.com/smallest_backend_ever');
+    await loadAllUsers();
+}
+
+
+let users = [];
+
 
 function openSignUpDialog() {
     let logInDialog = document.getElementById('log-in-box');
@@ -20,4 +23,51 @@ function closeSignUpDialog() {
 
     signUpDialog.classList.add('d-none');
     logInDialog.classList.remove('d-none');
+}
+
+
+async function signUp() {
+    let name = document.getElementById('signup-name');
+    let email = document.getElementById('signup-mail');
+    let password = document.getElementById('signup-password');
+
+    let user = {
+        'name': name.value,
+        'email': email.value,
+        'password': password.value
+    };
+
+    users.push(user);
+    await saveAllUsers();
+
+    name.value = '';
+    email.value = '';
+    password.value = '';
+
+    signUpNotification();
+    closeSignUpDialog();
+}
+
+
+async function saveAllUsers() {
+    await backend.setItem('users', JSON.stringify(users));
+}
+
+
+async function loadAllUsers() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+}
+
+async function deleteAllUsers() {
+    await backend.deleteItem('users');
+}
+
+
+function signUpNotification() {
+    document.getElementById('notification').classList.remove('d-none');
+
+    setTimeout(function () {
+        document.getElementById('notification').classList.add('d-none')
+    }, 3000)
 }
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function init() {
     includeHTML();
-    // setURL('https://kevin-wagner.developerakademie.net/smallest_backend_ever');
     setURL('https://kanbanboard.kev-wagner.com/smallest_backend_ever');
 }
