bca0b86b9774946bbeaafcb96c7cfc97f909d12f
Kevin Wagner <kevin_wagner4493@web.de>
2022-12-11 16:29:50 +0100

    finished project

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ async function init() {
     setURL('https://kanbanboard.kev-wagner.com/smallest_backend_ever');
 }
 
+
 // Log Out Dialog:
 function showLogOutDialog() {
     document.getElementById('dialog').classList.toggle('d-none');
